75966d0e7ee56df299b5f2c54b5b90b2b07b6e93
apurvam <apurva_mukherjee@mentor.com>
2018-03-28 11:34:54 +0530

    added new page for projects

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        projectMessage: 'PORTFOLIO for the projects'
+    })
+})
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
